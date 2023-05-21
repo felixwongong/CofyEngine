1d06e779b83f851e5288eab3a640491d9b56d91f
felixwongong <74048524+felixwongong@users.noreply.github.com>
2023-05-21 21:43:48 +0800

    add sample

---
+++ Samples/Script/CMBootstrapUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CM.UI;
+using CofyEngine;
+using CofyUI;
+using UnityEngine;
+
+namespace CM.Core
+{
+    public class CMBootstrapUI : BootstrapUI
+    {
+        public override Promise<List<GameObject>> LoadAll()
+        {
+            var uiRoot = UIRoot.Singleton;
+            
+            List<Promise<GameObject>> promises = new List<Promise<GameObject>>();
+
+            promises.Add(uiRoot.Bind<LoginUI>(LoadUIAssetAsync("LoginUI")));
+
+            return promises.Sequence();
+        }
+    }
+}
