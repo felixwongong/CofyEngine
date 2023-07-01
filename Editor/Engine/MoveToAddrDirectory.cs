2f5d70561eb1745f531ba07fe047f47e6c78df12
felixwongong <74048524+felixwongong@users.noreply.github.com>
2023-07-01 23:30:57 +0800

    add: editor function for easy move object

---
+++ Editor/Engine/MoveToAddrDirectory.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace CofyEngine.Editor
+{
+    public class MoveToAddrDirectory
+    {
+        [MenuItem("Assets/Move To/UI")]
+        static void MoveToUI()
+        {
+            if (Selection.objects is not { Length: > 0 }) return;
+            for (var i = 0; i < Selection.objects.Length; i++)
+            {
+                MoveToDirectory("Assets/Prefab/UI", Selection.objects[i]);
+            }
+        }
+
+        private static void MoveToDirectory(string targetDirPath, Object obj)
+        {
+            string sourcePath = AssetDatabase.GetAssetPath(obj);
+            string filename = Path.GetFileName(sourcePath);
+
+            string targetPath = Path.Combine($"{targetDirPath}/{filename}");
+
+            if (!Directory.Exists(targetDirPath))
+            {
+                FLog.Log(
+                    $"Target directory ({targetDirPath}) does not exist. Please create skeleton using CofyTool first");
+            }
+            else
+            {
+                AssetDatabase.MoveAsset(sourcePath, targetPath);
+            }
+        }
+    }
+}
