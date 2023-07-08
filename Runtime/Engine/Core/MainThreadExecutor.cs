b8b840438a7d8be8e7df73c7316eafc3ae76f17e
felixwongong <74048524+felixwongong@users.noreply.github.com>
2023-07-08 18:38:25 +0800

    add: main thread executer

---
+++ Runtime/Engine/Core/MainThreadExecutor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Engine.Util;
+using Action = Unity.Plastic.Newtonsoft.Json.Serialization.Action;
+
+namespace CofyEngine.Engine.Core
+{
+    public class MainThreadExecutor : MonoInstance<MainThreadExecutor>
+    {
+        private Queue<Action> _actionQueue;
+
+        private void Awake()
+        {
+            _actionQueue = new Queue<Action>();
+        }
+
+        private void Update()
+        {
+            while (_actionQueue.Count > 0)
+            {
+                try
+                {
+                    var action = _actionQueue.Dequeue();
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    FLog.LogException(new Exception("Exception occurs in Main Thread", ex));
+                }
+            }
+        }
+
+        public void QueueAction(Action action)
+        {
+            _actionQueue.Enqueue(action);
+        }
+    }
+}
--- Runtime/Engine/Util/PromiseSM/PromiseStateMachine.cs
+++ Runtime/Engine/Util/PromiseSM/PromiseStateMachine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CofyEngine.Engine.Core;
 using CofyEngine.Engine.Util.StateMachine;
 using UnityEngine;
 
@@ -12,20 +13,9 @@ namespace CofyEngine.Engine
 
         private Dictionary<Type, IPromiseState> _stateDictionary;
 
-        private Queue<Action> actionQueue = new Queue<Action>();
-
         private void Awake()
         {
             _stateDictionary = new Dictionary<Type, IPromiseState>();
-        }
-
-        private void Update()
-        {
-            while (actionQueue.Count > 0)
-            {
-                var action = actionQueue.Dequeue();
-                action();
-            }
         }
 
         protected void RegisterState(IPromiseState state)
@@ -39,7 +29,7 @@ namespace CofyEngine.Engine
             {
                 curState = _stateDictionary.Values.First(state => state is StateType);
             }
-            actionQueue.Enqueue(() => curState.StartContext(this));
+            MainThreadExecutor.instance.QueueAction(() => curState.StartContext(this));
         }
     }
 }
