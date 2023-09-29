bb3fec3702c144147f5b0e343bf33b152592812e
felixwongong <74048524+felixwongong@users.noreply.github.com>
2023-09-29 21:01:12 +0800

    create ui panel type

---
+++ Runtime/ui/Object/UIPanel.cs
@@ -0,0 +1,10 @@
+﻿namespace CofyUI
+{
+    public class UIPanel<TPanel>: UIInstance<TPanel>, IUIPanel
+    {
+        public virtual void ShowPanel(bool enable)
+        {
+            gameObject.SetActive(enable);
+        }
+    }
+}
