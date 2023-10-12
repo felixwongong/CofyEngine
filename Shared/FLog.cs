778e193e21b5e0eeb8a56eadf724d4dba843144c
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2023-10-12 09:33:52 +0800

    improve flog performance

--- Shared/FLog.cs
+++ Shared/FLog.cs
@@ -6,10 +6,12 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using UnityEngine;
+using WebSocketSharp;
 using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
 
 
-    public static class FLog
+public static class FLog
     {
         private static Dictionary<string, string> _propertyMap = new ();
         private static StringBuilder _sb = new ();
@@ -29,51 +31,43 @@ using Debug = UnityEngine.Debug;
             Debug.LogError(MakeLogString(inMsg, inObj));
         }
 
-        public static void LogObject(object inObj)
+        public static void Log<T>(T inObj)
         {
             Debug.Log(MakeLogString("", inObj));
         }
-        
+
         private static string MakeLogString(string inMsg = "", object inObj = null)
         {
-            
+
             Type type = new StackTrace().GetFrame(2).GetMethod().DeclaringType;
-            
-            try
-            {
-                var message = string.IsNullOrEmpty(inMsg) ? "" : string.Format("{0}\n", inMsg);
-                var obj = inObj == null ? "" : MakeFieldString(inObj);
-                while (type?.DeclaringType != null)
-                {
-                    type = type?.DeclaringType;
-                }
-
-                return string.Format("[{0}]: {1}{2}", type?.Name, message, obj);
-
-            }
-            catch (Exception e)
-            {
-                return string.Format("[{0}]: {1}", type?.Name, e);
-            }
-        }
-
-        private static string FormatString(string title, Dictionary<string, string> keyValue)
-        {
-            int maxKeyLength = keyValue.Keys.Select(k => k.Length).Max();
 
             _sb.Clear();
-
-            _sb.AppendLine(string.Format("{0}\n{1}", title, new string('-', title.Length)));
             
-            foreach (var (k, v) in keyValue)
+            while (type?.DeclaringType != null)
             {
-                _sb.AppendLine(string.Format("{0}: {1}", k.PadRight(maxKeyLength), v));
+                type = type?.DeclaringType;
             }
+            
+            _sb.AppendFormat("[{0}]: ", type?.Name);
+            if (!inMsg.IsNullOrEmpty()) _sb.AppendFormat("{0}\n", inMsg);
+            MakeFieldString(inObj, _sb);
 
             return _sb.ToString();
         }
 
-        private static string MakeFieldString(object obj)
+        private static void FormatString(string title, Dictionary<string, string> keyValue, in StringBuilder sb)
+        {
+            int maxKeyLength = keyValue.Keys.Select(k => k.Length).Max();
+
+            sb.AppendFormat("\n{0}\n{1}", title, new string('-', title.Length));
+            
+            foreach (var (k, v) in keyValue)
+            {
+                sb.AppendFormat("\n{0}: {1}", k.PadRight(maxKeyLength), v);
+            }
+        }
+
+        private static void MakeFieldString(object obj, in StringBuilder sb)
         {
             _propertyMap.Clear();
             
@@ -86,7 +80,7 @@ using Debug = UnityEngine.Debug;
                 _propertyMap[fieldInfo.Name] = fieldInfo.GetValue(obj).ToString();
             }
 
-            return FormatString(obj.GetType().ToString(), _propertyMap);
+            FormatString(obj.GetType().ToString(), _propertyMap, sb);
         }
 
         public static void LogException(Exception e)
