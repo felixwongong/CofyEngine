7abb8f721874ebe04f17e06b6c75048fc5be7e99
Felix Wong <felixwongyuenlam@gmail.com>
2023-11-17 18:55:08 +0800

    add more support & improve visual

--- Editor/engine/CofyDirectoryPath.cs
+++ Editor/engine/CofyDirectoryPath.cs
@@ -9,21 +9,27 @@ namespace CofyEngine.Editor
     {
     }
     
-    [CustomEditor(typeof(MonoBehaviour), true)]
+    [CustomEditor(typeof(UnityEngine.Object), true)]
     public class CofyDirectoryPathEditor : UnityEditor.Editor
     {
         private string _path;
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
-            
+            _OnInspectorGUI();
+        }
+
+        private void _OnInspectorGUI()
+        {
             var fields = target.GetType().GetFields();
-            
+
             for (var i = 0; i < fields.Length; i++)
             {
                 if (Attribute.GetCustomAttribute(fields[i], typeof(CofyDirectoryPath)) is not CofyDirectoryPath path) continue;
+
                 
-                if (GUILayout.Button("Open Directory"))
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(fields[i].Name);
+                if (GUILayout.Button(EditorGUIUtility.IconContent("_Popup"), EditorStyles.miniButton, GUILayout.ExpandWidth(false)))
                 {
                     _path = EditorUtility.OpenFolderPanel("Select Directory", "Assets", "");
                     if (string.IsNullOrEmpty(_path)) continue;
@@ -31,6 +37,8 @@ namespace CofyEngine.Editor
                     _path = _path.AsSpan(_path.IndexOf("Assets", StringComparison.Ordinal)).ToString();
                     fields[i].SetValue(target, _path);
                 }
+                GUILayout.TextField(_path);
+                GUILayout.EndHorizontal();
             }
         }
     }
