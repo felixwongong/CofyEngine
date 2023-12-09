739eb75f05a35dfe9054a672927ef1344a80a6d6
Felix Wong <felixwongyuenlam@gmail.com>
2023-12-10 00:04:45 +0800

    use assetmanager in bootstrapui

--- Runtime/engine/BootstrapState/BootstrapUI.cs
+++ Runtime/engine/BootstrapState/BootstrapUI.cs
@@ -1,48 +1,49 @@
 ﻿using System.Collections.Generic;
 using CofyUI;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 namespace CofyEngine
 {
     public abstract class BootstrapUI : IPromiseState<BootStateId>
     {
+        public BootStateId id => BootStateId.UI;
+        
         protected abstract Future<List<GameObject>> LoadAll();
 
         protected Future<GameObject> LoadUIAssetAsync(string path)
         {
-            return CofyAddressable.LoadAsset<GameObject>(PathResolver.GetAsset(AssetPath.UI, path));
+            return AssetManager.instance.LoadAsset<GameObject>(PathResolver.GetAsset(AssetPath.UI, path));
         }
 
         private Future<GameObject> LoadLocalUI(string path)
         {
-            var req = Resources.LoadAsync<GameObject>($"LocalUI/{path}");
-            return req.Future().TryMap(_ => (GameObject)req.asset);
+            return AssetManager.instance
+                .LoadAsset<GameObject>(string.Format("{0}/{1}", ConfigSO.inst.localPath, path), AssetLoadOption.ForceLoadLocal);
         }
 
-        public BootStateId id { get; }
         public void StartContext(IPromiseSM<BootStateId> sm, object param)
         {
             Future<List<GameObject>> loadFuture;
 
             LoadLocalUI("UIRoot").OnSucceed(uiRoot =>
-            {
-                Object.Instantiate(uiRoot);
-                UIRoot.instance.Bind<LoadingScreen>(LoadLocalUI("Loading/loading_panel"))
-                    .Then(future =>
-                    {
-                        var loadingScreen = LoadingScreen.instance;
-                        loadingScreen.SetGoActive(true);
-                        loadFuture = LoadAll();
-
-                        loadingScreen.MonitorProgress(loadFuture);
-
-                        loadFuture.Then(_ =>
+                {
+                    UIRoot.instance.Bind<LoadingScreen>(LoadLocalUI("loading_panel"))
+                        .Then(future =>
                         {
-                            FLog.Log("UI load finished.");
-                            sm.GoToState(BootStateId.UGS);
+                            var loadingScreen = LoadingScreen.instance;
+                            loadingScreen.SetGoActive(true);
+                            loadFuture = LoadAll();
+
+                            loadingScreen.MonitorProgress(loadFuture);
+
+                            loadFuture.Then(_ =>
+                            {
+                                FLog.Log("UI load finished.");
+                                sm.GoToState(BootStateId.UGS);
+                            });
                         });
-                    });
-            });
+                });
         }
 
         public void OnEndContext() { }
