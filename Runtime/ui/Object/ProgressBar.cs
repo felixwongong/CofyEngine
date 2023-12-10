f82ed47d2ad708450843c3c3eb6dab938575ac97
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2023-12-11 06:04:12 +0800

    added progress bar editor sldier

--- Runtime/ui/Object/ProgressBar.cs
+++ Runtime/ui/Object/ProgressBar.cs
@@ -1,17 +1,29 @@
 ﻿using System;
-using CofyEngine.Util;
 using UnityEngine;
 
 namespace CofyUI
 {
     public class ProgressBar : MonoBehaviour
     {
-        [SerializeField] private GameObject occupying;
+        [SerializeField] 
+        private GameObject occupying;
+        
+        [SerializeField] 
+        [Range(0, 1)]
+        private float _percentage;
 
         public void SetPercentage(float percent)
         {
             if (float.IsNaN(percent)) return;
+            _percentage = percent;
             occupying.transform.localScale = new Vector3(Math.Clamp(percent, 0f, 1f), 1, 1);
         }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            SetPercentage(_percentage);
+        }
+#endif
     }
 }
