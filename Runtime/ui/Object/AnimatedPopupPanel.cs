ddc4eb037c29c2856e41f52977166730aaad1070
Felix Wong <felixwongyuenlam@gmail.com>
2023-12-17 15:36:13 +0800

    added animatedpopuppanel

--- Runtime/ui/Object/AnimatedPopupPanel.cs
+++ Runtime/ui/Object/AnimatedPopupPanel.cs
@@ -2,16 +2,32 @@ using UnityEngine;
 
 namespace CofyUI
 {
-    public abstract class UIPopupPanel<TPanel>: UIInstance<TPanel>, IUIPanel
+    [ExecuteAlways]
+    //TODO: use name hash, add more editor param checking to ensure dun get wrong on designer
+    public abstract class AnimatedPopupPanel<TPanel>: UIInstance<TPanel>, IUIPanel
     {
         [SerializeField] protected Animator _animator;
-        
+        [SerializeField] protected string showAnim = "show_panel";
+        [SerializeField] protected string hideAnim = "hide_panel";
+
         public virtual void Show()
         {
+            _animator.Play(showAnim);
         }
 
         public virtual void Hide()
         {
+            _animator.Play(hideAnim);
         }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_animator == null)
+            {
+                _animator = GetComponentInChildren<Animator>();
+            }
+        }
+#endif
     }
 }
