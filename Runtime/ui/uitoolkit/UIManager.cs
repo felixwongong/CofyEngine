bdc1a3a12f8bdd0647a9be89d03ceeb45487e7f9
Felix Wong <felixwongyuenlam@gmail.com>
2023-12-19 14:27:04 +0800

    added bind logic

--- Runtime/ui/uitoolkit/UIManager.cs
+++ Runtime/ui/uitoolkit/UIManager.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using CofyUI;
-using Unity.Plastic.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,18 +11,37 @@ namespace CofyEngine
         public override bool persistent => true;
         
         [SerializeField] private UIDocument _document;
-        private Dictionary<System.Type, UIScreen> _uiMap = new();
+        private Dictionary<string, UIScreen> _uiMap = new();
+
+        protected override void Awake()
+        {
+            base.Awake();
+            UIScreen.assetLoader = uxmlPath => AssetManager.instance.LoadAsset<VisualTreeAsset>(uxmlPath, AssetLoadOption.ForceLoadLocal);
+        }
 
         public Future<T> Bind<T>(T screen, BindingOption option = BindingOption.None) where T: UIScreen
         {
             Promise<T> bindingPromise = new Promise<T>();
+
+            if (_uiMap.ContainsKey(screen.uxmlPath))
+                return Future<T>.failure(new Exception($"screen uxml path {screen.uxmlPath} already binded"));
+            else _uiMap[screen.uxmlPath] = screen;
+
+            switch (option)
+            {
+                case BindingOption.None: bindingPromise.Resolve(screen); break;
+                case BindingOption.Preload: 
+                    screen.LoadAsset(false).Then(_ => bindingPromise.Resolve(screen));
+                    break;
+                case BindingOption.Clone:
+                    screen.LoadAsset(true).Then(_ => bindingPromise.Resolve(screen));
+                    break;
+                default:
+                    bindingPromise.Reject(new Exception(string.Format("BindingOption {0} not supported", option)));
+                    break;
+            }
             
             return bindingPromise.future;
-        }
-
-        public void Test()
-        {
-            
         }
     }
 
@@ -36,13 +55,30 @@ namespace CofyEngine
     public abstract class UIScreen
     {
         protected internal string uxmlPath;
-        protected VisualElement screenRoot;
+        protected VisualTreeAsset asset;
+        protected VisualElement root;
 
-        public static Func<VisualTreeAsset, string> loadAsset;
+        public static Func<string, Future<VisualTreeAsset>> assetLoader;
         
         public UIScreen(string uxmlPath)
         {
             this.uxmlPath = uxmlPath;
+        }
+
+        public Future<bool> LoadAsset(bool instantiate)
+        {
+            if(assetLoader == null) return Future<bool>.failure(new NullReferenceException("assetLoader not set"));
+
+            var assetLoadFuture = assetLoader(uxmlPath);
+
+            if (!instantiate) return assetLoadFuture.TryMap(_ => true);
+            
+            return assetLoadFuture.TryMap(asset =>
+            {
+                this.asset = asset;
+                root = asset.CloneTree();
+                return true;
+            });
         }
     }
 }
